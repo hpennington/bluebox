dbb1ee17259e43348a7fc6eb5bced62d5eeea493
Hayden Pennington <haydenpennington@icloud.com>
2022-08-11 02:01:22 -0500

    Add iOS template project

---
+++ iOS/Bluebox/Shared/BlueboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueboxApp.swift
+//  Shared
+//
+//  Created by Hayden Pennington on 8/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BlueboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
