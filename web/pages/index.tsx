1d1ad1ab3e87e5ad9c6cbce619795c538ba9c483
Hayden Pennington <haydenpennington@icloud.com>
2022-08-13 11:25:03 -0500

    Add home page that redirects to blog for now

---
+++ web/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import {NextPage} from 'next'
+
+const Home: NextPage = () => (
+  <div>
+    <Head>
+      <meta httpEquiv="refresh" content="0.1; URL=/blog" />
+    </Head>
+  </div>
+)
+
+export default Home
