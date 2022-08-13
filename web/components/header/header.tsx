02c6e2367edf4aed2af13ecb744f73651e1aef31
Hayden Pennington <haydenpennington@icloud.com>
2022-08-12 20:02:23 -0500

    Use uppercase css module names

--- web/components/header/header.tsx
+++ web/components/header/header.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from './header.module.css'
+import styles from './Header.module.css'
 
 export const Header = () => {
   return (
--- web/components/posts/post-overview.tsx
+++ web/components/posts/post-overview.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import styles from './post-overview.module.css'
+import styles from './PostOverview.module.css'
 
 type Props = {
   title: string,
